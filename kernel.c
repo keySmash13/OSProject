d3463ef2a0d2ec8ece8237866bad0dabdddbf312
Abby Holdcraft <holdcraa@email.sc.edu>
2025-12-02 16:03:51 -0500

    basic CLI

--- kernel.c
+++ kernel.c
@@ -2,6 +2,7 @@ typedef unsigned char  uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int   uint32_t;
 typedef unsigned long  uint64_t;
+
 #define UART0_BASE 0x10000000
 #define UART_TX    0x00
 #define UART_RX    0x00
@@ -25,48 +26,96 @@ static inline char uart_getc(void) {
     return *rx;
 }
 
-
-// some code I wrote for a similar x86 project as a teenager.
-// adapted for this project.
+// read a string from UART
 void strin(char dest[], int len) {
-        unsigned char chr = 0;
-        char str[] = {0, 0};
-        int i;
-        for (i = 0;; i++) {
-                chr = uart_getc();
-                str[0] = chr;
-                uart_puts(str);
-                switch(chr) {
-                        case '\r':
-                                dest[i] = '\0';
-                                return;
-                        case 0x7f:
-                        case 0x08:
-                                uart_puts("\b \b");
-                                i--;
-                                break;
+    unsigned char chr;
+    int i = 0;
+
+    for (;;) {
+        chr = uart_getc();
+
+        switch(chr) {
+            case '\r':
+            case '\n':
+                dest[i] = '\0';
+                uart_puts("\r\n");
+                return;
+            case 0x7f: // backspace
+            case 0x08:
+                if (i > 0) {
+                    uart_puts("\b \b");
+                    i--;
                 }
-                if (i < len) dest[i] = chr;
+                break;
+            default:
+                if (i < len - 1) {
+                    dest[i++] = chr;
+                    uart_putc(chr);
+                }
         }
-        dest[i] = '\0';
+    }
 }
 
+// simple string compare
+int strcmp(const char *a, const char *b) {
+    while (*a && *b && *a == *b) a++, b++;
+    return (unsigned char)*a - (unsigned char)*b;
+}
+
+// simple string compare for prefix
+int strncmp(const char *a, const char *b, unsigned int n) {
+    unsigned int i;
+    for (i = 0; i < n; i++) {
+        if (a[i] != b[i] || a[i] == '\0' || b[i] == '\0')
+            return (unsigned char)a[i] - (unsigned char)b[i];
+    }
+    return 0;
+}
+
+// simple command functions
+void cmd_help(void) {
+    uart_puts("Available commands:\n");
+    uart_puts("  help - Show this help message\n");
+    uart_puts("  echo <text> - Echo text back\n");
+    uart_puts("  ls - List dummy files\n");
+}
+
+void cmd_echo(char *args) {
+    uart_puts(args);
+    uart_puts("\n");
+}
+
+void cmd_ls(void) {
+    uart_puts("file1.txt  file2.txt  kernel.bin\n");
+}
+
+// command dispatcher
+void run_command(char *input) {
+    // skip leading spaces
+    while (*input == ' ') input++;
+
+    if (strncmp(input, "help", 4) == 0) {
+        cmd_help();
+    } else if (strncmp(input, "echo", 4) == 0) {
+        char *args = input + 4;
+        while (*args == ' ') args++;
+        cmd_echo(args);
+    } else if (strcmp(input, "ls") == 0) {
+        cmd_ls();
+    } else if (*input != '\0') {
+        uart_puts("Unknown command. Type 'help' for a list.\n");
+    }
+}
 
 void kmain(void) {
     uart_puts("Please look at this window for input/output!\n");
     uart_puts("tiny-rv64-kernel: ready!\n");
 
     char buffer[100];
-    for (;;) {
-        strin(buffer, 100);
-        uart_putc('\n');
-    }
-/*
-    while (1) {
-        char c = uart_getc();  // blocking
-        uart_putc(c);
-        uart_puts("\n\r");
-    }
-    */
-}
 
+    for (;;) {
+        uart_puts("> ");      // prompt
+        strin(buffer, 100);   // read input
+        run_command(buffer);  // execute
+    }
+}
